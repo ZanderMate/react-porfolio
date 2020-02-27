3a15454d4a0aff40a3be4bd2b7deb0513df495c5
Alex Griep <alexgriep@hotmail.com>
2020-02-27 12:59:49 -0600

    redid portfolio again

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import React from 'react';
 import Navbar from './components/navbar';
 import PageContent from './components/PageContent';
-
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import AboutSection from './components/pages/AboutSection';
-import ContactSection from './components/pages/ContactSection';
-import ProjectSection from './components/pages/ProjectSection';
-import Home from './components/pages/Home';
+import Card from './components/Card'
+import Col from './components/Col';
+import Row from './components/Row';
+import AboutSection from './components/AboutSection';
+import ContactSection from './components/ContactSection';
+import ProjectSection from './components/ProjectSection';
+import ProjectTitle from './components/ProjectTitle';
+import Footer from './components/Footer';
 
 
 function App() {
   return (
     <div>
-      <Router>
-        < Navbar />
-        <PageContent>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={AboutSection} />
-          <Route exact path="/projects" component={ProjectSection} />
-          <Route path="/contact" component={ContactSection} />
-        </PageContent>
-      </Router>
+      < Navbar />
+      <PageContent>
+        <ProjectSection>
+          <ProjectTitle />
+          <Row>
+            <Col>
+              <Card
+                name="The Peace Project"
+                firstHref="https://github.com/ZanderMate/Project_01"
+                secondHref="https://ygajway.github.io/Project_01/index.html"
+                imgSrc="./assets/images/the-peace-project.png"
+              />
+              <Card
+                name="Pikachoose Pokedex"
+                firstHref="https://github.com/ZanderMate/teamikea"
+                secondHref="https://protected-spire-30180.herokuapp.com/"
+                imgSrc="./assets/images/pikachoose.png"
+              />
+            </Col>
+            <Col>
+              <Card
+                name="Random Password Generator"
+                firstHref="https://github.com/ZanderMate/zandermate.github.io/tree/master/random_password_generator"
+                secondHref="https://zandermate.github.io/random_password_generator/"
+                imgSrc="./assets/images/PWGenerator.png"
+              />
+              <Card
+                name="Weather Dashboard"
+                firstHref="https://github.com/ZanderMate/zandermate.github.io/tree/master/weather_dashboard"
+                secondHref="https://zandermate.github.io/weather_dashboard/"
+                imgSrc="./assets/images/weather-app.png"
+              />
+            </Col>
+            <Col>
+              <Card
+                name="Code Quizzes"
+                firstHref="https://github.com/ZanderMate/zandermate.github.io/tree/master/code_quizzes"
+                secondHref="https://zandermate.github.io/code_quizzes/"
+                imgSrc="./assets/images/code-quizzes.png"
+              />
+              <Card
+                name="Employee Directory"
+                firstHref="https://github.com/ZanderMate/employee-directory"
+                secondHref="https://employee-directory-bootcamp.netlify.com/"
+                imgSrc="./assets/images/employee-directory.png"
+              />
+            </Col>
+          </Row>
+        </ProjectSection>
+        <AboutSection />
+        <ContactSection />
+        <Footer />
+      </PageContent>
     </div >
   );
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import './style.css'
 
 
@@ -7,27 +6,19 @@ import './style.css'
 const Navbar = () => {
     return (
         <div>
-            {/* Sidebar/menu */}
+              {/* Sidebar/menu */}
             <nav className="w3-sidebar w3-bar-block w3-white w3-animate-left w3-text-grey w3-collapse w3-top w3-center" style={{ zIndex: 3, width: 300, fontWeight: "bold" }} id="mySidebar"><br />
-                <Link to="/" className='{window.location.pathname === "/" ? "nav-link active" : "nav-link"} w3-padding-64 w3-center brand-name'>
-                    <b>Alexander<br />Griep</b>
-                </Link>
-                <Link
-                    to="/projects"
-                    className='{window.location.pathname === "/projects" ? "nav-link active" : "nav-link"} w3-bar-item w3-button'>
-                    PROJECTS
-                </Link>
-                <Link
-                    to="/about"
-                    className='{window.location.pathname === "/about" ? "nav-link active" : "nav-link"} w3-bar-item w3-button'>
-                    ABOUT ME
-                </Link>
-                <Link
-                    to="/contact"
-                    className='{window.location.pathname === "/contact" ? "nav-link active" : "nav-link"} w3-bar-item w3-button'>
-                    CONTACT
-                </Link>
+                <h3 class="w3-padding-64 w3-center brand-name"><b>ALEXANDER<br />GRIEP</b></h3>
+                <a href="javascript:void(0)" onclick="w3_close()" className="w3-bar-item w3-button w3-padding w3-hide-large">CLOSE</a>
+                <a href="#projects" onclick="w3_close()" className="w3-bar-item w3-button">PROJECTS</a>
+                <a href="#about" onclick="w3_close()" class="w3-bar-item w3-button">ABOUT ME</a>
+                <a href="#contact" onclick="w3_close()" class="w3-bar-item w3-button">CONTACT</a>
             </nav>
+            {/* Top menu on small screens */}
+            <header class="w3-container w3-top w3-hide-large w3-white w3-xlarge w3-padding-16">
+                <span class="w3-left w3-padding">ALEX GRIEP</span>
+                <a href="javascript:void(0)" class="w3-right w3-button w3-white" onclick="w3_open()">☰</a>
+            </header>
             {/* Overlay effect when opening sidebar on small screens */}
             <div class="w3-overlay w3-hide-large w3-animate-opacity" onclick="w3_close()" style={{ cursor: "pointer" }} title="close side menu" id="myOverlay"></div>
         </div>
