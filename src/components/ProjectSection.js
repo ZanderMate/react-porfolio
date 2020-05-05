130a139bdd9d84337b5e40cbfdcb61de5adc670a
Alex Griep <alexgriep@hotmail.com>
2020-05-05 09:11:23 -0500

    updated background image in project section

--- src/components/ProjectSection.js
+++ src/components/ProjectSection.js
@@ -1,10 +1,11 @@
 import React from "react";
 import './style.css';
 import MyCarousel from './MyCarousel';
+import Background from '../images/project-background.jpg';
 
 const ProjectSection = () => {
     return (
-        <div className="w3-container w3-dark-grey w3-center w3-text-light-grey w3-padding-32" id="projects">
+        <div className="w3-container w3-center w3-text-light-grey w3-padding-32" style={{backgroundImage: `url(${Background})`}} id="projects">
             <h4 className="w3-center"><b>PROJECTS</b></h4>
             <MyCarousel />
         </div>
